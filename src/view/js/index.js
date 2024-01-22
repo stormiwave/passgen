7997ba12aa3a559edf27242b5feab38cab581542
Miriam <miriamserranotorrecillas@gmail.com>
2024-01-22 20:15:21 +0100

    copy and download

--- src/view/js/index.js
+++ src/view/js/index.js
@@ -1,10 +1,17 @@
 import { generatePassword } from './components/password-generator.js'
+import { copyPassword, downloadAsTxt } from './components/results.js'
 
 function init () {
   console.log('Initialization tasks being performed')
 
   const generateBtn = document.getElementById('generate')
   generateBtn.addEventListener('click', () => generatePassword())
+
+  const copyBtn = document.getElementById('copy')
+  copyBtn.addEventListener('click', () => copyPassword())
+
+  const downloadTxt = document.getElementById('download')
+  downloadTxt.addEventListener('click', () => downloadAsTxt())
 }
 
 init()
